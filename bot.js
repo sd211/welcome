75d411bc898c6256ba2d7c50df887173a2e7dce1
sd211 <41641489+sd211@users.noreply.github.com>
2018-07-25 00:38:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,30 +41,46 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 
 
 
- client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.get("456166860531040276");
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "Gr7..")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', 'chat');
     if (!channel) {
-        console.log("!the channel id it's not correct");
+        console.log("!channel fails");
         return;
     }
     if (member.id == client.user.id) {
         return;
     }
-    console.log('-');
+    console.log('made it till here!');
     var guild;
     while (!guild)
-        guild = client.guilds.get("456168212149829642");
+        guild = client.guilds.find("name", "Gr7..")
     guild.fetchInvites().then((data) => {
         data.forEach((Invite, key, map) => {
             var Inv = Invite.code;
             if (dat[Inv])
                 if (dat[Inv] < Invite.uses) {
- channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+ channel.send(` ♥ **تم دعوته من قبل ${Invite.inviter} ♥ `)            
  }
             dat[Inv] = Invite.uses;
-       
-       });
-    });
+        })
+    })
 });
   
 
