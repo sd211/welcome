b0a9b7337d4a5556327de41276c2bf3807b183c2
sd211 <41641489+sd211@users.noreply.github.com>
2018-07-25 00:22:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,46 +41,84 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 
 
 
-var dat = JSON.parse("{}");
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
-}
-client.on("ready", () => {
+  client.on("ready", () => {
+
     var guild;
+
     while (!guild)
-        guild = client.guilds.find("name", ".. Gr7")
+
+        guild = client.guilds.get("اي دي سيرفرك - Server id");
+
     guild.fetchInvites().then((data) => {
+
         data.forEach((Invite, key, map) => {
+
             var Inv = Invite.code;
+
             dat[Inv] = Invite.uses;
-        })
-    })
-})
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.find('name', 'chat');
-    if (!channel) {
-        console.log("!channel fails");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    var guild;
-    while (!guild)
-        guild = client.guilds.find("name", ".. Gr7")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                    console.log(3);
-                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
- channel.send(` ♥ **تم دعوته من قبل ${Invite.inviter}**  `)            
- }
-            dat[Inv] = Invite.uses;
-        })
-    })
+
+        });
+
+    });
+
 });
+
+ 
+
+ 
+
+ 
+
+client.on("guildMemberAdd", (member) => {
+
+    let channel = member.guild.channels.get("456168212149829642- Room id");
+
+    if (!channel) {
+
+        console.log("!the channel id it's not correct");
+
+        return;
+
+    }
+
+    if (member.id == client.user.id) {
+
+        return;
+
+    }
+
+    console.log('-');
+
+    var guild;
+
+    while (!guild)
+
+        guild = client.guilds.get("456166860531040276- Server id");
+
+    guild.fetchInvites().then((data) => {
+
+        data.forEach((Invite, key, map) => {
+
+            var Inv = Invite.code;
+
+            if (dat[Inv])
+
+                if (dat[Inv] < Invite.uses) {
+
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;       
+
+ }
+
+            dat[Inv] = Invite.uses;
+
+       
+
+       });
+
+    });
+
+});
+  
 
 
 client.login(process.env.BOT_TOKEN);
